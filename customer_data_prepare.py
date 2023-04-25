45ba2e86ed24feb3c89fb869d62c7fd4238bf17b
doratako <takodori@gmail.com>
2023-04-25 02:01:35 +0200

    refactor

--- customer_data_prepare.py
+++ customer_data_prepare.py
@@ -9,17 +9,13 @@ customer_latest_file = max(files, key=os.path.getctime)
 
 
 customer_df = pd.read_csv(customer_latest_file)
+customer_df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+customer_df.drop_duplicates(keep="first", inplace=True)
 
 if __name__ == "__main__":
     print(customer_df.info())
-
-    customer_df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-
     print(customer_df.isna().any())
-
     print(customer_df[customer_df.duplicated(keep="first")])
-    customer_df.drop_duplicates(keep="first", inplace=True)
-
     print(customer_df.head())
 
 
